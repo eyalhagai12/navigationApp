a0849ac51118dcb170c2a58f6c54c9758c1c8428
JonTP <jon9028@gmail.com>
2023-06-21 19:27:24 +0300

    can read and write external storage

--- app/src/main/java/com/example/stitching/activities/MainActivity.java
+++ app/src/main/java/com/example/stitching/activities/MainActivity.java
@@ -1,28 +1,68 @@
 package com.example.stitching.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.stitching.Logging.BackgroundLogging;
 import com.example.stitching.R;
 
 import boofcv.android.ConvertBitmap;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 
 public class MainActivity extends AppCompatActivity {
     boolean backgroundStarted = false;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkPermission()) {
+            Toast.makeText(MainActivity.this,"WE Have Permission",Toast.LENGTH_SHORT).show();   // WE have a permission just start your work.
+        } else {
+            requestPermission(); // Request Permission
+        }
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult( ActivityResult result ) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        if (Environment.isExternalStorageManager())
+                            Toast.makeText(MainActivity.this,"We Have Permission",Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "You Denied the permission", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "You Denied the permission", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
         Button stitchingButton = findViewById(R.id.stitchingButton);
         stitchingButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,11 +72,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button gpsButton = findViewById(R.id.SceneRecognitionButton);
-        gpsButton.setOnClickListener(new View.OnClickListener() {
+        Button sceneButton = findViewById(R.id.SceneRecognitionButton);
+        sceneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, GPSSceneRecognitionActivity.class);
+                Intent i = new Intent(MainActivity.this, SceneRecognitionActivity.class);
                 startActivity(i);
             }
         });
@@ -103,6 +143,43 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    private boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            int readCheck = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+            int writeCheck = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+            return readCheck == PackageManager.PERMISSION_GRANTED && writeCheck == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+    private String[] permissions = {READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE};
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Permission")
+                    .setMessage("Please give the Storage permission")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which ) {
+                            try {
+                                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                                intent.addCategory("android.intent.category.DEFAULT");
+                                intent.setData(Uri.parse(String.format("package:%s", new Object[]{getApplicationContext().getPackageName()})));
+                                activityResultLauncher.launch(intent);
+                            } catch (Exception e) {
+                                Intent intent = new Intent();
+                                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                                activityResultLauncher.launch(intent);
+                            }
+                        }
+                    })
+                    .setCancelable(false)
+                    .show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 30);
+        }
     }
 
 }
--- app/src/main/java/com/example/stitching/activities/SceneRecognitionActivity.java
+++ app/src/main/java/com/example/stitching/activities/SceneRecognitionActivity.java
@@ -1,49 +1,119 @@
 package com.example.stitching.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.stitching.R;
 
+import net.lingala.zip4j.ZipFile;
+
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 import boofcv.BoofVerbose;
 import boofcv.abst.scene.SceneRecognition;
 import boofcv.android.camera2.VisualizeCamera2Activity;
+import boofcv.io.UtilIO;
 import boofcv.io.recognition.RecognitionIO;
 import boofcv.misc.BoofMiscOps;
 import boofcv.struct.image.GrayU8;
 import boofcv.struct.image.ImageBase;
 import boofcv.struct.image.ImageType;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static androidx.activity.result.ActivityResultCallerKt.registerForActivityResult;
+
 public class SceneRecognitionActivity extends VisualizeCamera2Activity {
     private FrameLayout surface;
-    private ImageView bestMatch;
+    private ImageView bestMatchView;
     private SceneRecognition<GrayU8> recognizer;
+    public final static String MODEL = "model";
+    public final static String IMAGES = "images";
+
+    public final static String DATA_DIRECTORY = "scene_recognition";
+
+    // if pretraining own, should load it someplace and download it
+    public final static String MODEL_ADDRESS = "http://boofcv.org/notwiki/largefiles/scene_recognition_default38_inria_holidays.zip";
+
+    final Object lockRecognizer = new Object();
+
+    public SceneRecognitionActivity() {
+        super.bitmapMode = BitmapMode.NONE;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scene_recognition);
 
+
+
         surface = findViewById(R.id.liveVideoFeed);
-        bestMatch = findViewById(R.id.bestMatchImage);
+        bestMatchView = findViewById(R.id.bestMatchImage);
+
 
         // Tell it to process gray U8 images
         ImageType<GrayU8> imageType = ImageType.SB_U8;
         setImageType(imageType);
 
-        recognizer = RecognitionIO.downloadDefaultSceneRecognition(new File("downloaded_models"), imageType);
-        recognizer.setVerbose(System.out, BoofMiscOps.hashSet(BoofVerbose.RECURSIVE));
+        // on below line we are creating an image file and
+        // specifying path for the image file on below line.
+        File imgFile = new File("/storage/emulated/0/Pictures/scene_recognition/DJI_0486.JPG");
+
+
+        // on below line we are checking if the image file exist or not.
+        if (imgFile.exists()) {
+            // on below line we are creating an image bitmap variable
+            // and adding a bitmap to it from image file.
+            Bitmap imgBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+            // on below line we are setting bitmap to our image view.
+            bestMatchView.setImageBitmap(imgBitmap);
+        }
+
+//        recognizer = RecognitionIO.downloadDefaultSceneRecognition(new File("downloaded_models"), imageType);
+//        recognizer.setVerbose(System.out, BoofMiscOps.hashSet(BoofVerbose.RECURSIVE));
 
         startCamera(surface, null);
     }
+
+
+
 
     @Override
     protected void processImage(ImageBase image) {
 
     }
+
 }
